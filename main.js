fe3cdf4508d7a8d8a9ccecb602fdb77481ba72b2
Ashwin Chandran <ashwinchandran13@gmail.com>
2021-09-25 22:23:24 +0530

    commands

--- main.js
+++ main.js
@@ -6,10 +6,25 @@ if (env.error) {
     throw env.error;
 } 
 
+const prefix = '-';
+
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.once('ready', () => {
     console.log('nullcast dj is online');
+});
+
+client.on('message', message => {
+
+    console.log(message);
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+    
+    if (command === 'ping') {
+        message.channel.send('pong!')
+    }
 })
 
 client.login(process.env.LOGIN_TOKEN);
